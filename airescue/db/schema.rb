77fcab36707de075fdbfac3fe040e5b37d6163ca
Alejandra <mcolora2@eafit.edu.co>
2017-05-03 20:04:43 +0000

    Login Listo, se corrigio el error

--- airescue/db/schema.rb
+++ airescue/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170502184855) do
+ActiveRecord::Schema.define(version: 20170503182933) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,7 +41,7 @@ ActiveRecord::Schema.define(version: 20170502184855) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
+    t.string   "usuario",                default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
@@ -55,8 +55,9 @@ ActiveRecord::Schema.define(version: 20170502184855) do
     t.string   "permission_level"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
-    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
+    t.string   "email"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+    t.index ["usuario"], name: "index_users_on_usuario", unique: true, using: :btree
   end
 
   add_foreign_key "carbons", "nodes"
