4f17fda24198dac6c43d36aee7faf504ea294def
Lina Beltran <lbeltra1@eafit.edu.co>
2017-05-11 22:31:44 +0000

    graficas

--- airescue/app/controllers/graphics_controller.rb
+++ airescue/app/controllers/graphics_controller.rb
@@ -1,2 +1,12 @@
 class GraphicsController < ApplicationController
+    def index
+        @temperatures = Temperature.all
+    end
+    def show
+        puts @temp = Temperature.where(node_id: params[:id])
+        @dioxide = CarbonDioxide.where(node_id: params[:id])
+        @monoxide = CarbonMonoxide.where(node_id: params[:id])
+        @ozone = Ozone.where(node_id: params[:id])
+        @humed = Humidity.where(node_id: params[:id])
+    end
 end
