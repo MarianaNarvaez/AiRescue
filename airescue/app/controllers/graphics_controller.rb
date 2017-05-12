12206ea9615ccbb2fb2ce257f20c549e76716ac0
Lina Beltran <lbeltra1@eafit.edu.co>
2017-05-12 01:43:02 +0000

    graficas terminadas

--- airescue/app/controllers/graphics_controller.rb
+++ airescue/app/controllers/graphics_controller.rb
@@ -3,10 +3,16 @@ class GraphicsController < ApplicationController
         @temperatures = Temperature.all
     end
     def show
-        puts @temp = Temperature.where(node_id: params[:id])
+        @nombre = Node.find_by(id: params[:id]).name
+        @temp = Temperature.where(node_id: params[:id])
         @dioxide = CarbonDioxide.where(node_id: params[:id])
         @monoxide = CarbonMonoxide.where(node_id: params[:id])
         @ozone = Ozone.where(node_id: params[:id])
         @humed = Humidity.where(node_id: params[:id])
+        
+        @variables = ['dateTemperature', 'dataDioxide', 'dataMonoxide', 'dataOzone', 'dataHumidity']
+        for i in(0..9)
+        puts @variables[i]
+        end
     end
 end
