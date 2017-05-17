5cc46f3f14b02364d2637a6c8815cc56eae9a183
Lina Beltran <lbeltra1@eafit.edu.co>
2017-05-17 00:23:44 +0000

    nuevo controlador de graficas

--- airescue/app/controllers/graphics_controller.rb
+++ airescue/app/controllers/graphics_controller.rb
@@ -1,7 +1,4 @@
 class GraphicsController < ApplicationController
-    def index
-        @temperatures = Temperature.all
-    end
     def show
         @nombre = Node.find_by(id: params[:id]).name
         @temp = Temperature.where(node_id: params[:id])
@@ -9,10 +6,5 @@ class GraphicsController < ApplicationController
         @monoxide = CarbonMonoxide.where(node_id: params[:id])
         @ozone = Ozone.where(node_id: params[:id])
         @humed = Humidity.where(node_id: params[:id])
-        
-        @variables = ['dateTemperature', 'dataDioxide', 'dataMonoxide', 'dataOzone', 'dataHumidity']
-        for i in(0..9)
-        puts @variables[i]
-        end
     end
 end
