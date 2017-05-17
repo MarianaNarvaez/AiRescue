7262657ca43e6299482ebf96e196eb2fbcb38c2c
Mariana <mnarvae3@eafit.edu.co>
2017-05-17 01:19:33 +0000

    vista ShowDay agregada

--- airescue/app/controllers/graphics_controller.rb
+++ airescue/app/controllers/graphics_controller.rb
@@ -7,4 +7,12 @@ class GraphicsController < ApplicationController
         @ozone = Ozone.where(node_id: params[:id])
         @humed = Humidity.where(node_id: params[:id])
     end
+    def showDay
+       # @nombre = Node.find_by(id: params[:id]).name
+        @temp = Temperature.where(node_id: params[:id])
+        @dioxide = CarbonDioxide.where(node_id: params[:id])
+        @monoxide = CarbonMonoxide.where(node_id: params[:id])
+        @ozone = Ozone.where(node_id: params[:id])
+        @humed = Humidity.where(node_id: params[:id])
+    end
 end
